8be1fedf3bccc502bc3196f22bcf5ce66b25a563
neccodes <neccabrel22@gmail.com>
2025-10-16 00:59:08 +0400

    variables and manipulations

--- bin/variables.dart
+++ bin/variables.dart
@@ -5,10 +5,10 @@ void main(List<String> arguments) {
   final lowerCase = userInput.toLowerCase();
   print("lowerCase = $lowerCase");
   final upperCase = userInput.toUpperCase();
-  print("upperCase = $upperCase");
+  print('upperCase = $upperCase');
 
   // Trimming
-  const myUserInput = "  221B Baker Str. ";
+  const myUserInput = '   221B Baker Str. ';
   final trimmed = myUserInput.trim();
   print(trimmed);
 
@@ -17,8 +17,8 @@ void main(List<String> arguments) {
   final hours = time.inHours;
   final minutes = time.inMinutes % 60;
   final seconds = time.inSeconds % 60;
-  final timeString = "$hours:$minutes:$seconds";
-  print("timeString = $timeString");
+  final timeString = '$hours:$minutes:$seconds';
+  print('timeString = $timeString');
 
   final myTime = Duration(hours: 1, minutes: 2, seconds: 3);
   final myHours = myTime.inHours;
@@ -26,6 +26,26 @@ void main(List<String> arguments) {
   final mySeconds = '${myTime.inSeconds % 60}'.padLeft(2, "0");
   final myTimeString = '$myHours:$myMinutes:$mySeconds';
   print('myTimeString = $myTimeString');
+
+  // Splitting and Joining
+  const csvFileLine = 'Martin, Emma, 12, Paris, France';
+  final fields = csvFileLine.split(',');
+  print('fields = $fields');
+  final joined = fields.join('-');
+  print('joined = $joined');
+
+  //Replacing
+  const phrase = 'live and learn';
+  final withUnderScore = phrase.replaceAll(' ', '_');
+  print(withUnderScore);
+
+  // Exercises 1
+  const countryNames = 'France, USA, Germany, Benin, China, Mexico, Angola, Mongolia';
+  final myFields = countryNames.split(',');
+  print(myFields);
+  const myPhrase = "How's the Dart book going?:]";
+  final emoChanged = myPhrase.replaceAll(':]', '^^');
+  print(emoChanged);
 }
 
 
